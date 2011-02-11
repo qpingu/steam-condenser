qpingu/steam-condenser

commit a18698d181920b07e7e0ffab86badbb78c06423d

Restructured the server files.
- game_server: Keeps track of host and port outside of the socket. update_x_info returns the respective hash. update_player_info does not use rcon, opting for just the basic info. Use rcon_player_info instead if you desire the detailed information. to_s prints out the host:port, whilst all_info performs all of the queries and displays the info.

- goldsrc_server: No major changes.

- source_server: Additionally accepts local host/port to specify the TCP binding for rcon_socket. rcon_connect creates the socket and rcon_disconnect closes it. rcon_connect will call rcon_auth if a password is supplied.

- rcon_socket: Additionally accepts local host/port, passing these parameters to TCPSocket

ruby/lib/steam/servers/game_server.rb
@@ -15,11 +15,24 @@ require 'steam/packets/s2a_rules_packet'
 require 'steam/packets/s2c_challenge_packet'
 
 module GameServer
-
   REQUEST_CHALLENGE = 0
   REQUEST_INFO = 1
   REQUEST_PLAYER = 2
   REQUEST_RULES = 3
+  
+  attr_reader :host, :port
+
+  def initialize(remote_ip, remote_port = 27015)
+    raise ArgumentError.new('The remote port has to be a number greater than 0 and less than 65536.') unless port_number.to_i > 0 and port_number.to_i < 65536
+    
+    @host, @port, = remote_host, remote_port
+  end
+  
+  def init
+    update_ping
+    update_server_info
+    update_challenge_number
+  end
 
   # Returns the last measured response time of this server
   #
@@ -28,7 +41,7 @@ module GameServer
   #
   # Whenever you want to get a new value for the ping time call update_ping.
   def ping
-    update_ping if @ping.nil?
+    update_ping unless @ping
     @ping
   end
 
@@ -40,7 +53,7 @@ module GameServer
   # As the players and their scores change quite often be sure to update this
   # list regularly by calling update_player_info.
   def players(rcon_password = nil)
-    update_player_info(rcon_password) if @player_hash.nil?
+    update_player_info(rcon_password) unless @player_hash
     @player_hash
   end
 
@@ -55,7 +68,7 @@ module GameServer
   # this hash. But if you need to, you can achieve this by calling
   # update_rules_info.
   def rules
-    update_rules_info if @rules_hash.nil?
+    update_rules_info unless @rules_hash
     @rules_hash
   end
 
@@ -68,72 +81,12 @@ module GameServer
   # join or leave. As the latter changes can be monitored by calling
   # update_player_info, there's no need to call update_server_info very often.
   def server_info
-    update_server_info if @info_hash.nil?
+    update_server_info unless @info_hash
     @info_hash
   end
-
-  def handle_response_for_request(request_type, repeat_on_failure = true)
-    begin
-      case request_type
-        when GameServer::REQUEST_CHALLENGE then
-          request_packet = A2S_SERVERQUERY_GETCHALLENGE_Packet.new
-          expected_response = S2C_CHALLENGE_Packet
-        when GameServer::REQUEST_INFO then
-          request_packet = A2S_INFO_Packet.new
-          expected_response = S2A_INFO_BasePacket
-        when GameServer::REQUEST_PLAYER then
-          request_packet = A2S_PLAYER_Packet.new(@challenge_number)
-          expected_response = S2A_PLAYER_Packet
-        when GameServer::REQUEST_RULES then
-          request_packet = A2S_RULES_Packet.new(@challenge_number)
-          expected_response = S2A_RULES_Packet
-        else
-          raise SteamCondenserException.new("Called with wrong request type.")
-      end
-
-      send_request request_packet
-      response_packet = reply
-
-      if response_packet.kind_of? S2A_INFO_BasePacket
-        @info_hash = response_packet.info_hash
-      elsif response_packet.kind_of? S2A_PLAYER_Packet
-        @player_hash = response_packet.player_hash
-      elsif response_packet.kind_of? S2A_RULES_Packet
-        @rules_hash = response_packet.rules_hash
-      elsif response_packet.kind_of? S2C_CHALLENGE_Packet
-        @challenge_number = response_packet.challenge_number
-      else
-        raise SteamCondenserException.new("Response of type #{response_packet.class} cannot be handled by this method.")
-      end
-
-      unless response_packet.kind_of? expected_response
-        puts "Expected #{expected_response}, got #{response_packet.class}." if $DEBUG
-        handle_response_for_request(request_type, false) if repeat_on_failure
-      end
-    rescue TimeoutException
-      puts "Expected #{expected_response}, but timed out." if $DEBUG
-    end
-  end
-
-  def init
-    update_ping
-    update_server_info
-    update_challenge_number
-  end
-
-  def update_player_info(rcon_password = nil)
+  
+  def update_player_info
     handle_response_for_request GameServer::REQUEST_PLAYER
-
-    unless rcon_password.nil? or @player_hash.empty?
-      rcon_auth(rcon_password)
-      players = rcon_exec('status').split("\n")[7..-1]
-      players.pop if is_a? GoldSrcServer
-
-      players.each do |player|
-        player_data = self.class.split_player_status(player)
-        @player_hash[player_data[1]].add_info(*player_data) if @player_hash.key?(player_data[1])
-      end
-    end
   end
 
   def update_rules_info
@@ -155,19 +108,26 @@ module GameServer
     end_time = Time.now
     @ping = (end_time - start_time) * 1000
   end
+  
+  def rcon_player_info
+    handle_response_for_request GameServer::REQUEST_PLAYER
 
-  # Checks whether the listening port number of the server is in a valid range
-  def initialize(port_number = 27015)
-    unless port_number.to_i > 0 and port_number.to_i < 65536
-      raise ArgumentError.new('The listening port of the server has to be a number greater than 0 and less than 65536.')
+    if @player_hash
+      players = rcon_exec('status').split("\n")[7..-1]
+      players.pop if is_a? GoldSrcServer
+
+      players.each do |player|
+        player_data = self.class.split_player_status(player)
+        @player_hash[player_data[1]].add_info(*player_data) if @player_hash.key?(player_data[1])
+      end
     end
   end
 
-  def to_s
+  def all_info
     return_string = ''
 
-    return_string << "Ping: #{@ping}\n"
-    return_string << "Challenge number: #{@challenge_number}\n"
+    return_string << "Ping: #{ ping }\n"
+    return_string << "Challenge number: #{ @challenge_number }\n"
 
     unless @info_hash.nil?
       return_string << "Info:\n"
@@ -192,6 +152,10 @@ module GameServer
 
     return_string
   end
+  
+  def to_s
+    "#{ host }:#{ port }"
+  end
 
   protected
 
@@ -202,5 +166,47 @@ module GameServer
   def send_request packet
     @socket.send packet
   end
+  
+  def handle_response_for_request(request_type, repeat_on_failure = true)
+    begin
+      case request_type
+      when GameServer::REQUEST_CHALLENGE then
+        request_packet = A2S_SERVERQUERY_GETCHALLENGE_Packet.new
+        expected_response = S2C_CHALLENGE_Packet
+      when GameServer::REQUEST_INFO then
+        request_packet = A2S_INFO_Packet.new
+        expected_response = S2A_INFO_BasePacket
+      when GameServer::REQUEST_PLAYER then
+        request_packet = A2S_PLAYER_Packet.new(@challenge_number)
+        expected_response = S2A_PLAYER_Packet
+      when GameServer::REQUEST_RULES then
+        request_packet = A2S_RULES_Packet.new(@challenge_number)
+        expected_response = S2A_RULES_Packet
+      else
+        raise SteamCondenserException.new("Called with wrong request type.")
+      end
 
+      send_request request_packet
+      response_packet = reply
+
+      unless response_packet.kind_of? expected_response
+        puts "Expected #{expected_response}, got #{response_packet.class}." if $DEBUG
+        handle_response_for_request(request_type, false) if repeat_on_failure
+      end
+      
+      if response_packet.kind_of? S2A_INFO_BasePacket
+        return @info_hash = response_packet.info_hash
+      elsif response_packet.kind_of? S2A_PLAYER_Packet
+        return @player_hash = response_packet.player_hash
+      elsif response_packet.kind_of? S2A_RULES_Packet
+        return @rules_hash = response_packet.rules_hash
+      elsif response_packet.kind_of? S2C_CHALLENGE_Packet
+        return @challenge_number = response_packet.challenge_number
+      else
+        raise SteamCondenserException.new("Response of type #{response_packet.class} cannot be handled by this method.")
+      end
+    rescue TimeoutException
+      puts "Expected #{expected_response}, but timed out." if $DEBUG
+    end
+  end
 end

ruby/lib/steam/servers/source_server.rb
@@ -13,38 +13,19 @@ require 'steam/sockets/rcon_socket'
 require 'steam/sockets/source_socket'
 
 class SourceServer
-
   include GameServer
-  
-  attr_reader :ip, :port
 
-  # Splits the player status obtained with +rcon status+
-  def self.split_player_status(player_status)
-    player_data = player_status.match(/# *(\d+) +"(.*)" +(.*)/).to_a[1..-1]
-    player_data[2] = player_data[2].split
-    player_data.flatten!
-    player_data.delete_at(3)
-    player_data
-  end
+  attr_reader :local_host, :local_port
 
-  def initialize(ip_address, port_number = 27015)
-    @ip, @port = ip_address, port_number
-   
-    super port_number
-    @socket = SourceSocket.new ip, port
-    @rcon_socket = nil
+  def initialize(remote_ip, remote_port = 27015, local_host = nil, local_port = nil)
+    super remote_ip, remote_port, local_host, local_port
+    @socket = SourceSocket.new host, port
   end
   
   def rcon_connect(password = nil)
-    @rcon_socket = RCONSocket.new ip, port
-    rcon_auth password if password
-    
-    @rcon_socket != nil
-  end
-  
-  def rcon_disconnect
-    @rcon_socket.close if @rcon_socket
-    @rcon_socket = nil
+    @rcon_socket = RCONSocket.new host, port, local_host, local_port
+    rcon_auth(password) if password
+    true
   end
 
   def rcon_auth(password)
@@ -80,5 +61,14 @@ class SourceServer
     end
 
     response.strip
+  end
+  
+  # Splits the player status obtained with +rcon status+
+  def self.split_player_status(player_status)
+    player_data = player_status.match(/# *(\d+) +"(.*)" +(.*)/).to_a[1..-1]
+    player_data[2] = player_data[2].split
+    player_data.flatten!
+    player_data.delete_at(3)
+    player_data
   end
 end

ruby/lib/steam/servers/goldsrc_server.rb
@@ -7,9 +7,27 @@ require 'steam/servers/game_server'
 require 'steam/sockets/goldsrc_socket'
 
 class GoldSrcServer
-
   include GameServer
+  
+  attr_reader :hltv
 
+  # Should reorder arguments
+  def initialize(remote_host, remote_port = 27015, is_hltv = false)
+    super remote_host, remote_port
+    @hltv = is_hltv
+    
+    @socket = GoldSrcSocket.new host, port, hltv
+  end
+  
+  def rcon_auth(password)
+    @rcon_password = password
+    true
+  end
+
+  def rcon_exec(command)
+    @socket.rcon_exec(@rcon_password, command).strip
+  end
+  
   # Splits the player status obtained with +rcon status+
   def self.split_player_status(player_status)
     player_data = player_status.match(/# *(\d+) +"(.*)" +(\d+) +(.*)/).to_a[1..-1]
@@ -20,19 +38,4 @@ class GoldSrcServer
     player_data.delete_at(4)
     player_data
   end
-
-  def initialize(ip_address, port_number = 27015, is_hltv = false)
-    super port_number
-    @socket = GoldSrcSocket.new ip_address, port_number, is_hltv
-  end
-
-  def rcon_auth(password)
-    @rcon_password = password
-    true
-  end
-
-  def rcon_exec(command)
-    @socket.rcon_exec(@rcon_password, command).strip
-  end
-
 end

ruby/lib/steam/sockets/rcon_socket.rb
@@ -3,7 +3,6 @@
 #
 # Copyright (c) 2008-2011, Sebastian Staudt
 
-require 'ipaddr'
 require 'socket'
 require 'timeout'
 
@@ -13,20 +12,15 @@ require 'steam/packets/rcon/rcon_packet_factory'
 require 'steam/sockets/steam_socket'
 
 class RCONSocket
-
   include SteamSocket
 
-  def initialize(ip, port)
-    ip = IPSocket.getaddress(ip) unless ip.is_a? IPAddr
-
-    @ip     = ip
-    @port   = port
-    @socket = nil
+  def initialize(remote_host, remote_port, local_host = nil, local_port = nil)
+    @host, @port, @local_host, @local_port = remote_host, remote_port, local_host, local_port
   end
 
   def connect
     begin
-      timeout(@@timeout / 1000.0) { @socket = TCPSocket.new @ip, @port }
+      timeout(@@timeout / 1000.0) { @socket = TCPSocket.new @host, @port, @local_host, @local_port }
     rescue Timeout::Error
       raise TimeoutException
     end
@@ -34,7 +28,6 @@ class RCONSocket
 
   def send(data_packet)
     connect if @socket.nil? || @socket.closed?
-
     super
   end
 
@@ -60,5 +53,4 @@ class RCONSocket
 
     RCONPacketFactory.packet_from_data(packet_data)
   end
-
 end

commit 61b309e284f0f3102d93358cc79c308a104ae0c5

Fixed minor typos introduced in a18698d181920b07e7e0.

ruby/lib/steam/servers/game_server.rb
@@ -22,8 +22,8 @@ module GameServer
   
   attr_reader :host, :port
 
-  def initialize(remote_ip, remote_port = 27015)
-    raise ArgumentError.new('The remote port has to be a number greater than 0 and less than 65536.') unless port_number.to_i > 0 and port_number.to_i < 65536
+  def initialize(remote_host, remote_port = 27015)
+    raise ArgumentError.new('The remote port has to be a number greater than 0 and less than 65536.') unless remote_port.to_i > 0 and remote_port.to_i < 65536
     
     @host, @port, = remote_host, remote_port
   end

ruby/lib/steam/servers/source_server.rb
@@ -17,8 +17,10 @@ class SourceServer
 
   attr_reader :local_host, :local_port
 
-  def initialize(remote_ip, remote_port = 27015, local_host = nil, local_port = nil)
-    super remote_ip, remote_port, local_host, local_port
+  def initialize(remote_host, remote_port = 27015, local_host = nil, local_port = nil)
+    super remote_host, remote_port
+    @local_host, @local_port = local_host, local_port
+    
     @socket = SourceSocket.new host, port
   end
   